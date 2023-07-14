920bbeed3f624beeb04b2ba204ba7ec641f8b720
Ben Dixon <uk.bendixon@gmail.com>
2023-07-14 08:40:47 +0100

    website index and about changes

--- application.py
+++ application.py
@@ -17,6 +17,11 @@ def index():
    return render_template('index.html')
 
 
+@application.route('/about')
+def about():
+   return render_template('about.html')
+
+
 @application.route('/temp_local')
 def temp_local():
    
